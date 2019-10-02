8651d14a3c6bae55732a5e14c4e2e0e1b7df98d1
limelime <limelime@users.noreply.github.com>
2019-10-02 15:41:17 -0400

    Add employee.

---
+++ rest-api/rest-api/employee.php
@@ -0,0 +1,64 @@
+<?php
+class Employee{
+ 
+    private $conn;
+    private $table_name = "employee";
+ 
+    // Object properties.
+    public $id;
+    public $name;
+    public $role;
+ 
+    // Constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+	
+    
+	function readAll(){
+	 
+		// Query to insert record.
+		$query = "SELECT * FROM employee";
+
+		// Prepare query.
+		$stmt = $this->conn->prepare($query);
+	 
+		// Execute query.
+		if($stmt->execute()){
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			return $results;
+		}
+        else{
+            print_r($stmt->errorInfo());
+            return array();
+        }
+	}
+    
+	function create(){
+	 
+		// Query to insert record.
+		$query = "INSERT INTO employee(name, role) VALUES (:name, :role)";
+
+		// Prepare query.
+		$stmt = $this->conn->prepare($query);
+	 
+		// Sanitize inputs.
+		$this->name=htmlspecialchars(strip_tags($this->name));
+		$this->role=htmlspecialchars(strip_tags($this->role));
+	 
+		// Bind values.
+		$stmt->bindParam(":name", $this->name);
+		$stmt->bindParam(":role", $this->role);
+
+		// Execute query.
+		if($stmt->execute()){
+			return true;
+		}
+        else{
+            print_r($stmt->errorInfo());
+            return false;
+        }
+	}
+}
+
+?>
