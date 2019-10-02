d645297c97faf336a9b1f2819e0a61bbfeac9032
limelime <limelime@users.noreply.github.com>
2019-10-02 14:34:55 -0400

    Add database.php.

---
+++ rest-api/rest-api/database.php
@@ -0,0 +1,27 @@
+<?php
+class Database{
+ 
+    // Specify your own database credentials.
+    private $host = "localhost";
+    private $db_name = "rest_api_db";
+    private $username = "root2";
+    private $password = "password";
+    public $conn;
+ 
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host
+                                    . ";dbname=" . $this->db_name,
+                                    $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
